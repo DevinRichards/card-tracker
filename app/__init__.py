5d22e2dffafb3173c7e545c7174c75176cb710a4
DevinRichards <devinmrichards95@gmail.com>
2024-08-05 21:02:29 -0700

    project ready to start

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -14,7 +15,7 @@ login = LoginManager()
 login.login_view = 'auth.login'
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../frontend', static_url_path='/')
     app.config.from_object(Config)
 
     db.init_app(app)
@@ -25,9 +26,10 @@ def create_app():
     app.register_blueprint(routes.bp)
     app.register_blueprint(auth.bp, url_prefix='/auth')
 
-    return app
+    from .models import User
 
-@login.user_loader
-def load_user(user_id):
-    from .models import User  # Import here to avoid circular dependency
-    return User.query.get(int(user_id))
+    @login.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    return app
